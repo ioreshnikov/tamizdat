7f654949194d4576957a09f8c6afdae360f1a942
Ivan Oreshnikov <oreshnikov.ivan@gmail.com>
2019-04-27 00:27:30 +0200

    Adding an example settings file.

---
+++ tamizdat/settings.py
@@ -0,0 +1,9 @@
+raise NotImplementedError(
+    "please list the correct settings in tamizdat/settings.py")
+
+
+EMAIL_LOGIN = "tamizdatbot@example.com"
+EMAIL_PASSWORD = "secret email password"
+EMAIL_HOST = "stmp.example.com"
+EMAIL_PORT = 465
+TELEGRAM_TOKEN = "secret telegram token"
